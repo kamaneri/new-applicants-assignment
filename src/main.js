ef1ab91ac92461642a297c6862a4af3f6964278e
khurram maneri <khurram.maneri@gmail.com>
2023-11-20 12:36:44 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 // Add your JavaScript here
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('.button');
+
+    let currentInput = '';
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const value = button.textContent;
+
+            if (value === 'C') {
+                currentInput = '';
+            } else if (value === '=') {
+                try {
+                    currentInput = eval(currentInput).toString();
+                } catch (error) {
+                    currentInput = 'Error';
+                }
+            } else {
+                currentInput += value;
+            }
+
+            display.value = currentInput;
+        });
+    });
+});
